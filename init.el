d6d6e05d6c00a6375ee60aa88e5d84812b523f36
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2022-12-20 21:27:49 +0900

    enable melpa, added howm, ignore markdown-live-preview temprary file

--- init.el
+++ init.el
@@ -3,13 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(markdown-mode editorconfig vertico)))
+ '(package-selected-packages '(howm markdown-mode editorconfig vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Enable Melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
 ;; Enable vertico
 (use-package vertico
@@ -27,3 +35,11 @@
   :ensure t
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown"))
+
+;; howm
+(require 'howm)
+(setq howm-menu-lang 'ja)
+(global-set-key "\C-c,," 'howm-menu)
+(autoload 'howm-menu "howm" "Hitori Otegaru Wiki Modoki" t)
+'directory
+(setq howm-directory "~/Dropbox/howm")
